d0a481b9902166bf954505c96a4ed58bd02445da
Ellis Clarke <ellisbclarke03@gmail.com>
2021-10-12 15:15:07 +0100

    Initial Upload

---
+++ Orbit/orbit.py
@@ -0,0 +1,35 @@
+
+from turtle import *
+
+planetOne=[1, 1, 10]
+planetTwo = [2, 0.5, 30]
+planets = [planetOne, planetTwo]
+distanceScale = 200
+
+
+screen = Screen()
+screen.tracer(0)
+turtle = Turtle()
+turtle.setheading(90)
+
+
+def drawplanet(t,p):
+    t.forward(p[1] * distanceScale)
+    t.pendown()
+    t.circle(p[2])
+    t.penup()
+    t.goto(0, 0)
+
+
+
+def draw():
+    turtle.clear()
+    for planet in planets:
+        drawplanet(turtle, planet)
+    screen.update()
+    turtle.left(1)
+    screen.ontimer(draw, 200)  
+
+draw()
+
+screen.mainloop()
